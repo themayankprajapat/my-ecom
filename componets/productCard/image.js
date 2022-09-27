cf862aa7b32f38090df82b179aca77ff69f63d6e
unknown <prajapatmayank2905@gmail.com>
2022-09-27 21:05:53 +0530

    none

--- componets/productCard/image.js
+++ componets/productCard/image.js
@@ -1,61 +1,61 @@
 export const pics = [
     {
         id:1,
-        picName:"some1",
+        userName:"some1",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:2,
-        picName:"some2",
+        userName:"some2",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:3,
-        picName:"some3",
+        userName:"some3",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:4,
-        picName:"some4",
+        userName:"some4",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:5,
-        picName:"some5",
+        userName:"some5",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:6,
-        picName:"some6",
+        userName:"some6",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:7,
-        picName:"some7",
+        userName:"some7",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:8,
-        picName:"some8",
+        userName:"some8",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:9,
-        picName:"some9",
+        userName:"some9",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     },
     {
         id:10,
-        picName:"some10",
+        userName:"some10",
         price:120,
         discription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
     }
--- componets/productCard/productList.js
+++ componets/productCard/productList.js
@@ -1,6 +1,7 @@
 import Product from "./product";
 const ProductList = ({pics})=> {
-    const ProductComponent = pics.map((i)=>{
+    const ProductComponent = pics.map(i=>{
+        console.log(pics[i]);
     return <Product id={pics[i].id} name={pics[i].picName} price={pics[i].price} disc={pics[i].discription}/>});
     return (
         {ProductComponent}
